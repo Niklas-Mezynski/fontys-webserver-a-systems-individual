4578d425df449a6286f91b9966645802b0ea00c6
Niklas-Mezynski <n.mezynski@student.fontys.nl>
2023-05-20 10:35:06 +0200

    Round up the UI

--- packages/client/src/components/SensorLineChart.tsx
+++ packages/client/src/components/SensorLineChart.tsx
@@ -18,55 +18,60 @@ type Props = {
     date: Date;
     [x: string]: number | Date;
   }[];
+  title?: string;
 };
 
-export default function SensorLineChart({ data, dataLineSpecs }: Props) {
+export default function SensorLineChart({ data, dataLineSpecs, title }: Props) {
   console.log(dataLineSpecs);
   console.log(data);
 
   return (
-    <LineChart
-      width={Math.min(screen.width, 800)}
-      height={Math.min(screen.width * 0.65, 520)}
-      data={data}
-    >
-      <XAxis
-        dataKey="date"
-        tickFormatter={(value: Date) =>
-          value
-            .toLocaleString('de-DE', {
-              day: '2-digit',
-              month: '2-digit',
-              hour: '2-digit',
-              minute: '2-digit',
-            })
-            .replace(',', '')
-        }
-      />
-      <YAxis />
-      <CartesianGrid stroke="#ccc" />
-      <Tooltip
-        labelFormatter={(label: Date) =>
-          label
-            .toLocaleString('de-DE', {
-              day: '2-digit',
-              month: '2-digit',
-              year: 'numeric',
-              hour: '2-digit',
-              minute: '2-digit',
-            })
-            .replace(',', '')
-        }
-      />
-      <Legend />
-      {dataLineSpecs.map((lineSpec) => (
-        <Line
-          type="monotone"
-          dataKey={lineSpec.dataKey}
-          name={lineSpec.label}
-          stroke={lineSpec.strokeColor || '#B3B388'}
+    <>
+      <h2 className="text-2xl mb-1 mt-3">{title}</h2>
+      <LineChart
+        width={Math.min(screen.width, 800)}
+        height={Math.min(screen.width * 0.65, 520)}
+        data={data}
+      >
+        <XAxis
+          dataKey="date"
+          tickFormatter={(value: Date) =>
+            value
+              .toLocaleString('de-DE', {
+                day: '2-digit',
+                month: '2-digit',
+                hour: '2-digit',
+                minute: '2-digit',
+              })
+              .replace(',', '')
+          }
         />
-      ))}
-    </LineChart>
+        <YAxis />
+        <CartesianGrid stroke="#ccc" />
+        <Tooltip
+          labelFormatter={(label: Date) =>
+            label
+              .toLocaleString('de-DE', {
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric',
+                hour: '2-digit',
+                minute: '2-digit',
+              })
+              .replace(',', '')
+          }
+        />
+        <Legend />
+        {dataLineSpecs.map((lineSpec, index) => (
+          <Line
+            key={index}
+            type="monotone"
+            dataKey={lineSpec.dataKey}
+            name={lineSpec.label}
+            stroke={lineSpec.strokeColor || '#B3B388'}
+          />
+        ))}
+      </LineChart>
+    </>
   );
 }
